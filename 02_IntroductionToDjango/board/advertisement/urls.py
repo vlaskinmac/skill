2ccb5ef5f9bc0375a1de343beb84925b78d711ff
kangelina86@mail.ru <Vfrcvfrc1>
2021-08-22 11:21:38 +0300

    2

--- 02_IntroductionToDjango/board/advertisement/urls.py
+++ 02_IntroductionToDjango/board/advertisement/urls.py
@@ -4,8 +4,8 @@ from .import views
 
 urlpatterns = [
     path('', views.advertisement_list, name='advertisement_list'),
-    path('course_1/', views.course_1, name='advertisement/course_1.html'),
-    path('course_2/', views.course_1, name='advertisement/course_2.html'),
-    path('course_3/', views.course_1, name='advertisement/course_3.html'),
-    path('course_4/', views.course_1, name='advertisement/course_4.html'),
+    path('course_1/', views.course_1, name='course_1'),
+    path('course_2/', views.course_2, name='course_2'),
+    path('course_3/', views.course_3, name='course_3'),
+    path('course_4/', views.course_4, name='course_4'),
 ]
--- 02_IntroductionToDjango/board/advertisement/views.py
+++ 02_IntroductionToDjango/board/advertisement/views.py
@@ -7,3 +7,15 @@ def advertisement_list(request, *args, **kwargs):
 
 def course_1(request, *args, **kwargs):
     return render(request, 'advertisement/course_1.html')
+
+
+def course_2(request, *args, **kwargs):
+    return render(request, 'advertisement/course_2.html')
+
+
+def course_3(request, *args, **kwargs):
+    return render(request, 'advertisement/course_3.html')
+
+
+def course_4(request, *args, **kwargs):
+    return render(request, 'advertisement/course_4.html')
